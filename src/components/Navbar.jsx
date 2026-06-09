2004742f5040a9bb61cafaa4152da11fbb51d521
Paras Sahu <parastop5468@gmail.com>
2026-06-09 14:10:27 +0530

    tech

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ import { Link } from "react-router-dom";
 
   return (
     <>
-      <nav className="fixed top-0 left-0 w-full z-50 backdrop-blur-xl bg-black/60 border-b border-white/10">
+      <nav className="fixed top-0 left-0 w-full z-50 backdrop-blur-xl bg-black/90 border-b border-white/10">
         <div className="max-w-7xl mx-auto px-5">
           <div className="flex items-center justify-between h-20">
 
@@ -28,7 +28,7 @@ import { Link } from "react-router-dom";
               </div>
 
               <h1 className="text-2xl font-extrabold bg-gradient-to-r from-cyan-400 via-purple-400 to-pink-400 bg-clip-text text-transparent">
-                PlayHub
+                PlayOut
               </h1>
             </Link>
 
