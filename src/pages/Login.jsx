b1813135b3d4582bc1744767c71d071b938702f8
Paras Sahu <paras2115s@gmail.com>
2026-06-15 23:48:48 +0530

    login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form action="">
+        <div>
+            <label htmlFor="">Email</label>
+            <input type="text"
+            placeholder='enter your email'
+            name='email' />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Login
